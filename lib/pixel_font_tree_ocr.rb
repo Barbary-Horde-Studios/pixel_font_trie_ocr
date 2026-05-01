d5195c636e0daf3da007e26a4661395a821f874e
Robert Ferney <rob@ferney.org>
2026-05-01 05:47:07 -0600

    bundle gem pixel_font_trie_ocr --test=rspec --mit --coc

---
+++ lib/pixel_font_tree_ocr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pixel_font_tree_ocr/version"
+
+module PixelFontTreeOcr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pixel_font_tree_ocr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PixelFontTreeOcr
+  VERSION = "0.1.0"
+end
