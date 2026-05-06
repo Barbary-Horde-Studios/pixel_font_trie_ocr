e39793a9785ee1fcdf0b539301c1f005cf97d371
Robert Ferney <rob@ferney.org>
2026-05-05 23:57:57 -0600

    autocrop input images

--- lib/pixel_font_trie_ocr/image_column_extractor.rb
+++ lib/pixel_font_trie_ocr/image_column_extractor.rb
@@ -2,18 +2,49 @@
 
 class PixelFontTrieOCR
   class ImageColumnExtractor
-    attr_reader :image, :threshold
+    attr_reader :image, :threshold, :min_x, :max_x, :min_y, :max_y
 
     def initialize(image, threshold: 38_000)
       @image = image
       @threshold = threshold
+      @min_x = image.columns
+      @max_x = 0
+      @min_y = image.rows
+      @max_y = 0
+    end
+
+    def black(x, y)
+      @min_x = [min_x, x].min
+      @max_x = [max_x, x].max
+      @min_y = [min_y, y].min
+      @max_y = [max_y, y].max
     end
 
     def extract
+      auto_select
+      return [0, 0] unless black_pixels?
+
+      crop
       trim_masks(masks)
     end
 
     private
+
+    def auto_select
+      image.each_pixel do |pixel, x, y|
+        black(x, y) if black?(pixel)
+      end
+    end
+
+    def black_pixels?
+      min_x > max_x # no black pixels
+    end
+
+    def crop
+      @width = max_x - min_x + 1
+      @height = max_y - min_y + 1
+      @image = image.crop(min_x, min_y, width, height, true)
+    end
 
     def width
       @width ||= image.columns
